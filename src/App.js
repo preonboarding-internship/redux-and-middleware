9dabd06a978931ae52d06113bf424daddad116a4
Yeonuk Hwang <younuk23@gmail.com>
2023-01-11 22:33:34 +0900

    FEAT: starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter } from "./store/counter";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // const increase = () => {
-  //   setCount((prev) => prev + 1);
-  // };
-
-  // get state from redux store
-  const count = useSelector((store) => {
-    return store.counter.value;
-  });
-  const dispatch = useDispatch();
+  const [count, setCount] = useState(0);
 
   const increase = () => {
-    const increaseAction = increaseCounter();
-    dispatch(increaseAction);
+    setCount((prev) => prev + 1);
   };
 
   return (
